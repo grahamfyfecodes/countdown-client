5ba88ac234d725a08aaba191bdfd26523f218663
grahamfyfecodes <67826423+grahamfyfecodes@users.noreply.github.com>
2021-10-19 15:47:41 +0100

    moving pc

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,44 @@
-function searchForGame() {
-	
+var ws;
+
+function searchForGame() {	
 	gameId = 101
 	letters = "teiroadnce"
 	
 	location.href = 'game.html?gameId=' + gameId + "&letters=" + letters;
+}
+
+function connect() {
+	var socket = new WebSocket('ws://localhost:8080/greeting');
+	ws = Stomp.over(socket);
+
+	ws.connect({}, function(frame) {
+		ws.subscribe("/user/queue/errors", function(message) {
+			alert("Error " + message.body);
+		});
+
+		ws.subscribe("/topic/user", function(message) {
+			showGreeting(message.body);
+		});
+	}, function(error) {
+		alert("STOMP error " + error);
+	});
+}
+
+function disconnect() {
+    if (ws != null) {
+        ws.close();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function send() {
+	var data = JSON.stringify({
+		'playerId' : 999
+	})
+	ws.send("/app/playerQueue", {}, data);
+}
+
+function showGreeting(message) {
+	console.log(message);
 }
